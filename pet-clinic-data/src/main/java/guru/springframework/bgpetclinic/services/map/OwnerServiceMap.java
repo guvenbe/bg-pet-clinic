9777d68701aa540ca576fe9d9d1e599aa680b0c0
guvenbe <guvenbe@gmail.com>
2019-01-07 01:45:23 -0500

    DataLoad added

--- pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/map/OwnerServiceMap.java
@@ -3,10 +3,11 @@ package guru.springframework.bgpetclinic.services.map;
 
 import guru.springframework.bgpetclinic.model.Owner;
 import guru.springframework.bgpetclinic.services.CrudService;
+import guru.springframework.bgpetclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -30,5 +31,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
