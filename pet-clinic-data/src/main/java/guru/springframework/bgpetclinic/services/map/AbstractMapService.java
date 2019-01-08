275e6abd071fa93319bdf904447afc6752340469
Bora Guven <bora.guven@gm.com>
2019-01-07 20:34:37 -0500

    added vet and owner thymeleaf pages and controllers Added auto Key gen

--- pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/map/AbstractMapService.java
@@ -2,14 +2,17 @@ package guru.springframework.bgpetclinic.services.map;
 
 import guru.springframework.bgpetclinic.model.BaseEntity;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -19,8 +22,16 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object !=null){
+            if(object.getId() ==null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw  new RuntimeException("Object cannot be null");
+        }
+
         return object;
     }
 
@@ -30,6 +41,16 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
 
     void deleteById(ID id) {
         map.remove(id);
+    }
+
+    private Long getNextId(){
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch(NoSuchElementException e){
+            nextId=1L;
+        }
+        return nextId;
     }
 
 }
