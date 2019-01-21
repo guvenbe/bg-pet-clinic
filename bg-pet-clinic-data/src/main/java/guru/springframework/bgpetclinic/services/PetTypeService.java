0114cb0e80c327859fb995bafe71a8ab5f004986
Bora Guven <guvenbe@gmail.com>
2019-01-21 16:08:06 -0500

    added pettpye service

---
+++ bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package guru.springframework.bgpetclinic.services;
+
+import guru.springframework.bgpetclinic.model.PetType;
+
+public interface PetTypeService extends  CrudService<PetType, Long> {
+
+}
