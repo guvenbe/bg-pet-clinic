ac41ae64a1d32cdb38747e00bb9df1382ef5b6dd
Bora Guven <guvenbe@gmail.com>
2019-01-21 20:10:29 -0500

    added PetTypes to Bootsraps Dataloader

--- bg-pet-clinic-web/src/main/java/guru/springframework/bgpetclinic/bootstrap/DataLoader.java
+++ bg-pet-clinic-web/src/main/java/guru/springframework/bgpetclinic/bootstrap/DataLoader.java
@@ -2,8 +2,10 @@ package guru.springframework.bgpetclinic.bootstrap;
 
 
 import guru.springframework.bgpetclinic.model.Owner;
+import guru.springframework.bgpetclinic.model.PetType;
 import guru.springframework.bgpetclinic.model.Vet;
 import guru.springframework.bgpetclinic.services.OwnerService;
+import guru.springframework.bgpetclinic.services.PetTypeService;
 import guru.springframework.bgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,14 +16,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private  final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
        this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
 
