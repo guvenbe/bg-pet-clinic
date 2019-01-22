efdbf1983fdc894e3fc148c24c14a21f6ecdf929
Bora Guven <guvenbe@gmail.com>
2019-01-22 02:15:05 -0500

    Added Specialteies map, service

---
+++ bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.bgpetclinic.services;
+
+import guru.springframework.bgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
