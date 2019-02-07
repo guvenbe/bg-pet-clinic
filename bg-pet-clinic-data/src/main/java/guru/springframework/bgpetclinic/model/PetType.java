94cf63a191a2b2513b115c2796f1f8d4e9a558d6
Bora Guven <guvenbe@gmail.com>
2019-02-07 00:40:55 -0500

    updated owner with JPA mappings

--- bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/model/PetType.java
+++ bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/model/PetType.java
@@ -1,7 +1,15 @@
 package guru.springframework.bgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends  BaseEntity{
-    String name;
+
+    @Column(name = "name")
+    private String name;
 
     public String getName() {
         return name;
