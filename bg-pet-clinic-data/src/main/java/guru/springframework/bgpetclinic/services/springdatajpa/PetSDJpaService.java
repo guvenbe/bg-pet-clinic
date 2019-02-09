088ffdb98a6628aea1c6d9711a84adb0036c1eab
Bora Guven <guvenbe@gmail.com>
2019-02-09 15:24:52 -0500

    add Vet JPA service and add @Service to PetSDJpaService

--- bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/springdatajpa/PetSDJpaService.java
+++ bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -3,10 +3,14 @@ package guru.springframework.bgpetclinic.services.springdatajpa;
 import guru.springframework.bgpetclinic.model.Pet;
 import guru.springframework.bgpetclinic.model.repositories.PetRepository;
 import guru.springframework.bgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class PetSDJpaService implements PetService {
 
     private final PetRepository petRepository;
