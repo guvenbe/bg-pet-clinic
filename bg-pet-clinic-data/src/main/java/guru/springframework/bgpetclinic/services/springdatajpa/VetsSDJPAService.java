cd82e00c57d765e0e5f0a9db160bbe536ed3e89f
Bora Guven <guvenbe@gmail.com>
2019-02-09 13:14:34 -0500

    added vet jpa service

---
+++ bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/springdatajpa/VetsSDJPAService.java
@@ -0,0 +1,47 @@
+package guru.springframework.bgpetclinic.services.springdatajpa;
+
+import guru.springframework.bgpetclinic.model.Vet;
+import guru.springframework.bgpetclinic.model.repositories.VetRepository;
+import guru.springframework.bgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetsSDJPAService implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetsSDJPAService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet vet){
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
