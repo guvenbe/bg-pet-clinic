a38a1a8ad648d53e0c17f8d12f4e098ac3654d0f
Bora Guven <guvenbe@gmail.com>
2019-02-16 17:37:19 -0500

    Implemeneted project lombok closes #22

--- bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/model/Speciality.java
+++ bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/model/Speciality.java
@@ -1,20 +1,19 @@
 package guru.springframework.bgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "specialties")
 public class Speciality extends BaseEntity{
     @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
