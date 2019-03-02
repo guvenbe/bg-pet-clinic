3cceffb46d0aa7f111ad1f361df95f5eae72c146
Bora Guven <guvenbe@gmail.com>
2019-03-02 09:45:42 -0500

    added maven dependecies for JUNIT5 and converted from JUNIT4. Closes #4

---
+++ bg-pet-clinic-web/src/test/java/guru/springframework/bgpetclinic/BgPetClinicApplicationTests.java
@@ -0,0 +1,18 @@
+package guru.springframework.bgpetclinic;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class BgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
