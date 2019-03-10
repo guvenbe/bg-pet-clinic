78a66f4be5f8b6248d41c52ae04e5ecbba1fa9b2
Bora Guven <guvenbe@gmail.com>
2019-03-10 00:19:41 -0500

    implement find owner. Closes #25

--- bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/model/repositories/OwnerRepository.java
+++ bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/model/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package guru.springframework.bgpetclinic.model.repositories;
 import guru.springframework.bgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
--- bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/OwnerService.java
+++ bg-pet-clinic-data/src/main/java/guru/springframework/bgpetclinic/services/OwnerService.java
@@ -2,9 +2,11 @@ package guru.springframework.bgpetclinic.services;
 
 import guru.springframework.bgpetclinic.model.Owner;
 
+import java.util.List;
 
 
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
 
+    List<Owner> findAllByLastNameLike(String lastName);
 }
